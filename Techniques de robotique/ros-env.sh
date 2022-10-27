8e51c99ede6c45c148e06bd17faf932e7ba53a24
Ian Avelar Peixoto <avelarian@gmail.com>
2022-10-27 10:13:49 +0200

    :zap: Improved robotique module

--- Techniques de robotique/ros-env.sh
+++ Techniques de robotique/ros-env.sh
@@ -1,3 +1,9 @@
 xhost + 127.0.0.1
 
+CONTAINER_ID=$(docker ps -q -l)
+SRC_PATH="./TP01/"
+DEST_PATH="~/catkin_ws/"
+
+docker cp $SRC_PATH $CONTAINER_ID:$DEST_PATH
+
 docker exec -it "dev-ros-container" /bin/bash  -c "export DISPLAY=host.docker.internal:0 && cd ~/catkin_ws/ && source devel/setup.sh && bash"
